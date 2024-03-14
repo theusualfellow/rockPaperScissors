9cb4e0a56988a0ab187d6889d73fa4538dab0702
sandeep <sandeepsingh23ok@gmail.com>
2024-03-14 01:21:00 -0300

    Complete game and better written code

--- script.js
+++ script.js
@@ -4,29 +4,51 @@ function getComputerChoice(){
     let randomChoice= choices[randomNumber];
     return randomChoice;
 }
-function playGame(playerSelection, computerSelection){
+
+function playRound(playerSelection, computerSelection){
     let playerSelectionClean = playerSelection.toLowerCase();
     console.log(playerSelectionClean);
     if  (playerSelectionClean != "rock" &&
         playerSelectionClean != "paper" &&
         playerSelectionClean != "scissors"){
-            console.log("invalid input");
+            throw"invalid input";
         }
     else if(playerSelectionClean == computerSelection){
         return"it's a tie";
     }
-    else if(playerSelectionClean=="paper" && computerSelection=="rock"){
+    else if(
+        playerSelectionClean=="paper" && computerSelection=="rock" ||
+        playerSelectionClean=="scissors" && computerSelection=="paper" ||
+        playerSelectionClean=="rock" && computerSelection=="scissors"
+    ){
         return"you win";
-    }
-    else if(playerSelectionClean=="scissors" && computerSelection=="paper"){
-        return"you win";
-    }
-    else if(playerSelectionClean=="rock" && computerSelection=="scissors"){
-        return "you win";
     }
     else{
         return"you lose";
     }
 }
 
-playGame("Scissors", getComputerChoice());
+
+function playGame(){
+    
+    let counter = 0;
+    for(let i=0; i<5; i++){
+        
+        let choice = prompt("enter your choice");
+        
+        let result = playRound(choice, getComputerChoice());
+        console.log(result);
+        if (result == "you win"){
+            
+            counter++;
+        }
+        else{
+            continue;
+        }
+        
+    }
+    return counter
+    
+}
+
+console.log("your score is "+playGame());
