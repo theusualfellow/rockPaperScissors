a106abf1b6f46699faa9c94bd18e68163cd772e4
sandeep <sandeepsingh23ok@gmail.com>
2024-03-14 00:41:34 -0300

    Add return instead of console log

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function getComputerChoice(){
     const choices = ["rock", "paper", "scissors"];
     let randomNumber = Math.floor(Math.random()*3);
     let randomChoice= choices[randomNumber];
-    console.log(randomChoice);
     return randomChoice;
 }
 function playGame(playerSelection, computerSelection){
@@ -14,19 +13,19 @@ function playGame(playerSelection, computerSelection){
             console.log("invalid input");
         }
     else if(playerSelectionClean == computerSelection){
-        console.log("it's a tie");
+        return"it's a tie";
     }
     else if(playerSelectionClean=="paper" && computerSelection=="rock"){
-        console.log("you win");
+        return"you win";
     }
     else if(playerSelectionClean=="scissors" && computerSelection=="paper"){
-        console.log("you win");
+        return"you win";
     }
     else if(playerSelectionClean=="rock" && computerSelection=="scissors"){
-        console.log("you win");
+        return "you win";
     }
     else{
-        console.log("you lose");
+        return"you lose";
     }
 }
 
